9b0cb462fd5607e60cc6cf7601a321344eb40dad
igitego Begton <igitegoaimebegton@gmail.com>
2026-06-02 17:47:15 +0200

    Delete backend-project directory

--- backend-project/middleware/auth.js
+++
@@ -1,8 +0,0 @@
-// Simple session validation middleware
-const verifySession = (req, res, next) => {
-  // In this simplified version, we just pass through
-  // Frontend manages session with localStorage
-  next();
-};
-
-module.exports = { verifySession };
